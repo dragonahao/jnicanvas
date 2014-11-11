0022feaa17ba754e5d4e210ff436ec5456e16c65
Bradley J. Plohr <bplohr@comcast.net>
2014-11-11 03:24:08 -0700

    Improved conformance with Java coding conventions.

--- tests/guiJava/Space2d.java
+++ tests/guiJava/Space2d.java
@@ -1,25 +1,25 @@
 public class Space2d
 {
+    public static enum Space2dMode { VERT, HORIZ };
+
+    public static final int OPTION1 = 0x1;
+    public static final int OPTION2 = 0x2;
+    public static final int OPTION3 = 0x4;
+
+    public static final double INTENSITY_SCALE = 100.;
+    public static final double INTENSITY_MIN = 0.;
+    public static final double INTENSITY_MAX = 100.;
+
+    public static final double DEPTH_SCALE = 10.;
+    public static final double DEPTH_MIN = 0.;
+    public static final double DEPTH_MAX = 10.;
+
     public Space2dMode mode;
     public int option;
     public double intensity;
     public double depth;
     public double height;
     public double width;
-
-    public enum Space2dMode { VERT, HORIZ };
-
-    public int OPTION1 = 0x1;
-    public int OPTION2 = 0x2;
-    public int OPTION3 = 0x4;
-
-    public double INTENSITY_SCALE = 100.;
-    public double INTENSITY_MIN = 0.;
-    public double INTENSITY_MAX = 100.;
-
-    public double DEPTH_SCALE = 10.;
-    public double DEPTH_MIN = 0.;
-    public double DEPTH_MAX = 10.;
 
     public Space2d() {
         mode = Space2dMode.HORIZ;
@@ -31,12 +31,12 @@ public class Space2d
     }
 
     public static void main(final String[] args) {
-        Space2d space_2d = new Space2d();
-        System.out.println("mode = " + space_2d.mode);
-        System.out.println("option = " + space_2d.option);
-        System.out.println("intensity = " + space_2d.intensity);
-        System.out.println("depth = " + space_2d.depth);
-        System.out.println("height = " + space_2d.height);
-        System.out.println("width = " + space_2d.width);
+        Space2d space2d = new Space2d();
+        System.out.println("mode = " + space2d.mode);
+        System.out.println("option = " + space2d.option);
+        System.out.println("intensity = " + space2d.intensity);
+        System.out.println("depth = " + space2d.depth);
+        System.out.println("height = " + space2d.height);
+        System.out.println("width = " + space2d.width);
     }
 }
--- tests/guiJava/Space2dModesChoice.java
+++ tests/guiJava/Space2dModesChoice.java
@@ -5,28 +5,28 @@ import javax.swing.*;
 class Space2dModesChoice extends Choice {
     private static final String label = "Modes:";
     private static final String[] strings = { "Vertical", "Horizontal" };
-    private Space2d space_2d_;
+    private Space2d space2d;
 
-    public Space2dModesChoice(JPanel parent, Space2d space_2d) {
+    public Space2dModesChoice(JPanel parent, Space2d space2d_) {
         super(parent, label, strings,
-                space_2d.mode == Space2d.Space2dMode.VERT ? 0 : 1);
-        space_2d_ = space_2d;
+                space2d_.mode == Space2d.Space2dMode.VERT ? 0 : 1);
+        space2d = space2d_;
     }
 
     public void actionPerformed(ActionEvent event) {
-	space_2d_.mode = (event.getActionCommand() == strings[0]
+	space2d.mode = (event.getActionCommand() == strings[0]
                 ? Space2d.Space2dMode.VERT : Space2d.Space2dMode.HORIZ);
 	System.out.println("Space2dModesChoice::actionPerformed(): mode = "
-                + space_2d_.mode);
+                + space2d.mode);
     }
 
     public static void main(final String[] args) {
         JFrame frame = new JFrame();
         JPanel panel = new JPanel();
         frame.getContentPane().add(panel);
-        Space2d space_2d = new Space2d();
-        Space2dModesChoice space_2d_modes_choice
-                = new Space2dModesChoice(panel, space_2d);
+        Space2d space2d = new Space2d();
+        Space2dModesChoice space2dModesChoice
+                = new Space2dModesChoice(panel, space2d);
         frame.setSize(300, 75);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
