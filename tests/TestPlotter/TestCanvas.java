57122577c3b97278156bfaaadf7736cc05cf392b
Edson Landim de Almeida <edsonlan@impa.br>
2014-11-13 17:59:13 -0200

    Removing unused field

--- tests/TestPlotter/TestCanvas.java
+++ tests/TestPlotter/TestCanvas.java
@@ -4,17 +4,10 @@ public class TestCanvas extends Canvas {
     static {
         System.loadLibrary("Paint");
     }
+    
+   
 
-    
-    private boolean changeColor;
-
-    public void changeColor(boolean changeColor) {
-        this.changeColor = changeColor;
-    }
-    
-    
-    
-    
+     
     
     public native void paint(Graphics g);
     
