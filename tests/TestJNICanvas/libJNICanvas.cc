b16b4bcb3c98d99b052ec4d346ddf2ccab74f60e
Bradley J. Plohr <bplohr@comcast.net>
2014-11-16 02:41:23 -0700

    Separated make_current() from construct_graphics().

--- tests/TestJNICanvas/libJNICanvas.cc
+++ tests/TestJNICanvas/libJNICanvas.cc
@@ -12,8 +12,10 @@ static JAWT_DrawingSurface* ds;
 static JAWT_DrawingSurfaceInfo* dsi;
 static JAWT_X11DrawingSurfaceInfo* dsi_x11;
 static GC gc;
+static GLXContext glc;
 
-static void construct_graphics(JNIEnv *env, jobject canvas)
+static void
+construct_graphics(JNIEnv *env, jobject canvas)
 {
     std::cerr << "entering construct_graphics()\n";
     std::cerr << "is_graphics_constructed = "
@@ -66,11 +68,11 @@ static void construct_graphics(JNIEnv *env, jobject canvas)
 //    swa.colormap = cmap;
 //    swa.event_mask = ExposureMask | KeyPressMask;
 
-    GLXContext glc = glXCreateContext(dsi_x11->display, vi, NULL, GL_TRUE);
+    glc = glXCreateContext(dsi_x11->display, vi, NULL, GL_TRUE);
+
+    make_current();
 
     glShadeModel(GL_SMOOTH);
-
-    glXMakeCurrent(dsi_x11->display, dsi_x11->drawable, glc);
 
     clear();
 
@@ -79,7 +81,8 @@ static void construct_graphics(JNIEnv *env, jobject canvas)
     is_graphics_constructed = true;
 }
 
-static void destruct_graphics(void)
+static void
+destruct_graphics(void)
 {
     std::cerr << "entering destruct_graphics()\n";
     std::cerr << "is_graphics_constructed = "
@@ -102,19 +105,28 @@ static void destruct_graphics(void)
     is_graphics_constructed = false;
 }
 
-void clear(void)
+void
+make_current(void)
+{
+    glXMakeCurrent(dsi_x11->display, dsi_x11->drawable, glc);
+}
+
+void
+clear(void)
 {
     glClearColor(0.0, 0.0, 0.0, 1.0);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     swap_buffers();
 }
 
-void swap_buffers(void)
+void
+swap_buffers(void)
 {
     glXSwapBuffers(dsi_x11->display, dsi_x11->drawable);
 }
 
-void lock(void)
+void
+lock(void)
 {
     jint lock = ds->Lock(ds);
     if((lock & JAWT_LOCK_ERROR) != 0) {
@@ -125,24 +137,28 @@ void lock(void)
     }
 }
 
-void unlock(void)
+void
+unlock(void)
 {
     ds->Unlock(ds);
 }
 
-JNIEXPORT jint JNI_OnLoad(JavaVM *vm, void *reserved) {
+JNIEXPORT jint
+JNI_OnLoad(JavaVM *vm, void *reserved) {
     std::cerr << "entering JNI_OnLoad()\n";
     return JNI_VERSION_1_2;
 }
 
 /*
-JNIEXPORT void JNICALL Java_JNICanvas_initialize(JNIEnv *env, jobject canvas) {
+JNIEXPORT void JNICALL
+Java_JNICanvas_initialize(JNIEnv *env, jobject canvas) {
     std::cerr << "entering Java_JNICanvas_initialize()\n";
     construct_graphics(env, canvas);
 }
 */
 
-JNIEXPORT void JNICALL Java_JNICanvas_paint(JNIEnv* env, jobject canvas,
+JNIEXPORT void JNICALL
+Java_JNICanvas_paint(JNIEnv* env, jobject canvas,
         jobject graphics) {
     std::cerr << "entering Java_JNICanvas_paint()\n";
     // count on paint() being called before any drawing is done
@@ -151,16 +167,19 @@ JNIEXPORT void JNICALL Java_JNICanvas_paint(JNIEnv* env, jobject canvas,
 }
 
 /*
-JNIEXPORT void JNICALL Java_JNICanvas_dispose(JNIEnv *, jobject) {
+JNIEXPORT void JNICALL
+Java_JNICanvas_dispose(JNIEnv *, jobject) {
     std::cerr << "entering Java_JNICanvas_dispose()\n";
     destruct_graphics();
 }
 */
 
-JNIEXPORT void JNICALL Java_JNICanvas_segment(JNIEnv *env, jobject canvas,
+JNIEXPORT void JNICALL
+Java_JNICanvas_segment(JNIEnv *env, jobject canvas,
         jdouble x1, jdouble y1, jdouble x2, jdouble y2)
 {
     std::cerr << "entering Java_JNICanvas_segment()\n";
+    make_current();
     lock();
 
     glColor4d(255, 0, 0, 0);
--- tests/TestJNICanvas/libJNICanvas.h
+++ tests/TestJNICanvas/libJNICanvas.h
@@ -1,6 +1,7 @@
 #ifndef _libJNICanvas_h
 #define _libJNICanvas_h
 
+void make_current(void);
 void clear(void);
 void swap_buffers(void);
 void lock(void);
